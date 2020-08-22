5d5a6375bc11e2ac9e3558a97edff19bd5a3c961
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-08-22 11:08:13 +0530

    Completed the add item to cart functionality with quantity count

--- src/components/shopping-cart-item/shopping-cart-item.component.jsx
+++ src/components/shopping-cart-item/shopping-cart-item.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { addItemToCart } from "../../redux/shopping-cart/shopping-cart.actions";
 
 import trashcan from "../../asset/trashcan.svg";
 
 import "./shopping-cart-item.styles.scss";
 
-const ShoppingCartItem = ({ cartItem }) => {
-  const { name } = cartItem;
+const ShoppingCartItem = ({ cartItem, addItemToCart }) => {
+  const { name, quantity } = cartItem;
   return (
     <div className="cart-item">
       <p className="item-name">{name}</p>
@@ -14,11 +17,18 @@ const ShoppingCartItem = ({ cartItem }) => {
           <img src={trashcan} alt="Trashcan Icon" />
         </div>
         <div className="tag-box reduce">&#8211;</div>
-        <div className="tag-box quantity">3 pcs</div>
-        <div className="tag-box add">&#43;</div>
+        <div className="tag-box quantity">{quantity} pcs</div>
+        <div className="tag-box add" onClick={() => addItemToCart(cartItem)}>
+          &#43;
+        </div>
       </div>
     </div>
   );
 };
 
-export default ShoppingCartItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItemToCart: (item) => dispatch(addItemToCart(item)),
+  };
+};
+export default connect(null, mapDispatchToProps)(ShoppingCartItem);
--- src/redux/shopping-cart/shopping-cart.utils.js
+++ src/redux/shopping-cart/shopping-cart.utils.js
@@ -1,5 +1,24 @@
 export const addItemToCart = (cart, item) => {
-  console.log(cart);
-  console.log(item);
-  return [...cart, item];
+  // Ffnf if the item already exists in cart
+  const isItemPresent = cart.find((currentItem) => currentItem.id === item.id);
+
+  // if already exists, increase quanity by one for matched item with id
+  if (isItemPresent) {
+    return cart.map((currentItem) => {
+      if (currentItem.id === item.id) {
+        return {
+          ...currentItem,
+          quantity: currentItem.quantity + 1,
+        };
+      } else {
+        return currentItem;
+      }
+    });
+  }
+  // else return the same items with quantity set to 1
+  else {
+    console.log("Triggered!");
+    const newCart = [...cart, { ...item, quantity: 1 }];
+    return newCart;
+  }
 };
